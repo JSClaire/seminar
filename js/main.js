effab6ee21dcdb46bdcba00e1d63776acea1c07d
jisoo <sonjisuu@gmail.com>
2023-10-23 12:10:42 +0900

    클레어 작업

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mainSwiper", {
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+});
