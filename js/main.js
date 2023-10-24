56d935a0a749db9d6678fed4c7bb9c8a45bfc103
jisoo <sonjisuu@gmail.com>
2023-10-24 16:42:04 +0900

    메인배너까지 작업완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 200) {
+        $('header').addClass('scroll');
+    }
+    else if($(window).scrollTop() <= 0){
+        $('header').removeClass('scroll');
+    }
+});
+
 var swiper = new Swiper(".mainSwiper", {
     autoplay: {
         delay: 2500,
@@ -5,6 +14,14 @@ var swiper = new Swiper(".mainSwiper", {
     },
     pagination: {
         el: ".swiper-pagination",
-        type: "progressbar",
+        type: "fraction",
     },
+    loop:true,
 });
+
+var swiper = new Swiper(".navi", {
+    slidesPerView: "auto",
+});
+
+
+
